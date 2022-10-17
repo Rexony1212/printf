98da0cd778d03d5471f152fe49e30acc7de8984c
Rex Ifendu <ifenduusimrex@gmail.com>
2022-10-17 15:18:26 -0700

    printf.c file

--- _printf.c
+++ _printf.c
@@ -15,3 +15,16 @@ va_list args;
 
 if (format == NULL)
 return (-1);
+
+size = _strlen(format);
+if (size <= 0)
+return (0);
+
+va_start(args, format);
+size = handler(format, args);
+
+_putchar(-1);
+va_end(args);
+
+return (size);
+}
